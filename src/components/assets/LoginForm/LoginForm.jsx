6747a1e0f06f5b49113a758a1dbc5209c95b5b8b
Poorani <poorani4402@gmail.com>
2024-04-25 10:13:30 +0530

    awfAf

--- src/components/assets/LoginForm/LoginForm.jsx
+++ src/components/assets/LoginForm/LoginForm.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import './LoginForm.css';
+
 const LoginForm = () => {
     return (
-        <div>LoginForm</div>
+        <div className='wrapper'>
+           <form action ="">
+               <h1>Login</h1>
+               <div classname="input-box"> 
+               <input type="text" placeholder='Username' required />
+               </div>
+
+               <div classname="input-box"> 
+               <input type="password" placeholder='Password' required />
+               </div>
+           </form>
+           </div>
     );
 };
  export default LoginForm;
