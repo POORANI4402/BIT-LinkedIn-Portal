a6736098e2161715b976b97cf4a6f3a90eb4a9c1
Poorani <poorani4402@gmail.com>
2024-04-25 11:31:27 +0530

    kjgf

--- src/components/assets/LoginForm/LoginForm.jsx
+++ src/components/assets/LoginForm/LoginForm.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './LoginForm.css';
+import {FaUser,FaLock} from "react-icons/fa";
 
 const LoginForm = () => {
     return (
@@ -8,10 +9,22 @@ const LoginForm = () => {
                <h1>Login</h1>
                <div classname="input-box"> 
                <input type="text" placeholder='Username' required />
+               <FaUser className='icon'/>
                </div>
 
                <div classname="input-box"> 
                <input type="password" placeholder='Password' required />
+               <FaLock className='icon'/>
+               </div>
+
+               <div classname="rember-forget">
+                <label><input type="checkbox"/ >Remember me</label>
+                 <a href="#">Forget Password</a>
+               </div>
+
+               <button type="submit">Login</button>
+               <div className="register-link">
+                <p>Dont Have an Account <a href="#">Register</a></p>
                </div>
            </form>
            </div>
